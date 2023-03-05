0ebd1ac1fffa1c70a9a64d539d8db6a3546ba45e
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-03-05 15:17:59 +0100

    Connection to database

---
+++ src/db/mongo.rs
@@ -0,0 +1,23 @@
+use mongodb::{Client, options::ClientOptions, Database, error::Result as MongoResult };
+use dotenv::dotenv;
+
+pub struct Mongo;
+
+impl Mongo {
+    pub async fn init() -> MongoResult<Database> {
+
+        dotenv().ok();
+
+        let dbms = std::env::var("DBMS").expect("No DBMS env variable");
+        let db_name = std::env::var("DB_NAME").expect("No DB_NAME env variable");
+        let host = std::env::var("DB_HOST").expect("No DB_HOST env variable");
+        let port = std::env::var("DB_PORT").unwrap_or("27017".into());
+
+        let fqdm = format!("{dbms}://{host}:{port}");
+
+        let client_options = ClientOptions::parse(fqdm).await?;
+        let client = Client::with_options(client_options)?;
+        let db = client.database(db_name.as_str());
+        Ok(db)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod api;
 mod server;
+mod db;
 
 use axum::Router;
 
 use server::server::Server;
+use db::mongo::Mongo;
 use api::auth::config::configure as auth;
 
 #[tokio::main]
 async fn main() {
+
+    let _db = Mongo::init().await;
 
     let routes = Router::new().nest("/auth", auth());
 
