4c2b8d2d2ba5c7aae2f4ade3ba0ebe0ad1fa981b
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-03-13 18:58:29 +0100

    New test

--- tests/profile.rs
+++ tests/profile.rs
@@ -50,4 +50,33 @@ mod tests {
 
         assert_eq!(response.status(), StatusCode::OK);
     }
+
+    #[tokio::test]
+    async fn profile_update_validates_request() {
+        let app = app().await;
+        let uri = "/api/profile/update";
+
+        let auth_header = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImV4YW1wbGVAZ21haWwuY29tIn0.mvztvf67TauQ_3PIlM8YYmP0vb3d6yFE1olXVRUtlx0";
+
+        /* This operation should fail since password must be at least 8 chars long */
+
+        let body = json!(
+            {
+                "password": "123456"
+            }
+        );
+
+        let response = app
+            .oneshot(
+                Request::put(uri)
+                    .header(http::header::CONTENT_TYPE, "application/json")
+                    .header(http::header::AUTHORIZATION, auth_header)
+                    .body(Body::from(body.to_string()))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
 }
